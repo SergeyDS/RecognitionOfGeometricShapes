1bdd16e60025cdee4eac84050b6a3b4600b57ee2
Sergey <sergey_1c@mail.ru>
2021-05-30 20:28:52 +0700

    RecognitionOfGeometricShapes

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace RecognitionOfGeometricShapes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.инструментыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.найтиФигурыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.инструментыToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(772, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(372, 410);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(391, 28);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(372, 410);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // инструментыToolStripMenuItem
+            // 
+            this.инструментыToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.найтиФигурыToolStripMenuItem});
+            this.инструментыToolStripMenuItem.Name = "инструментыToolStripMenuItem";
+            this.инструментыToolStripMenuItem.Size = new System.Drawing.Size(95, 20);
+            this.инструментыToolStripMenuItem.Text = "Инструменты";
+            // 
+            // найтиФигурыToolStripMenuItem
+            // 
+            this.найтиФигурыToolStripMenuItem.Name = "найтиФигурыToolStripMenuItem";
+            this.найтиФигурыToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.найтиФигурыToolStripMenuItem.Text = "Найти фигуры";
+            this.найтиФигурыToolStripMenuItem.Click += new System.EventHandler(this.найтиФигурыToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Geometric Shapes";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem инструментыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem найтиФигурыToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu;
+using Emgu.Util;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+
+namespace RecognitionOfGeometricShapes
+{
+    
+    
+    public partial class Form1 : Form
+    {
+        // Распознаем геометрические фигуры на фото
+        private Image<Bgr, byte> inputImage = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult res = openFileDialog1.ShowDialog();
+                if (res == DialogResult.OK)
+                {
+                    inputImage = new Image<Bgr, byte>(openFileDialog1.FileName);
+                    pictureBox1.Image = inputImage.Bitmap;
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Изображение не выбрано ! ", "Ошибка !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Ошибка !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void найтиФигурыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Image<Gray, byte> grayImage = inputImage.SmoothGaussian(5).Convert<Gray,byte>().ThresholdBinaryInv(new Gray(230),new Gray(255));
+            VectorOfVectorOfPoint conturs = new VectorOfVectorOfPoint();
+
+            Mat hierarchy = new Mat();
+
+            CvInvoke.FindContours(grayImage, conturs, hierarchy, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+            for (int i = 0; i < conturs.Size; i++)
+            {
+                double perimetr = CvInvoke.ArcLength(conturs[i],true);
+                VectorOfPoint approximation = new VectorOfPoint();
+
+                CvInvoke.ApproxPolyDP(conturs[i], approximation, 0.04 * perimetr ,true);
+                CvInvoke.DrawContours(inputImage,conturs,i,new MCvScalar(0,0,255),2);
+
+
+                //Moments moments = CvInvoke.Moments(conturs[i]);
+                MCvMoments moments = CvInvoke.Moments(conturs[i]);
+
+                int x = (int)(moments.M10 / moments.M00);
+                int y = (int)(moments.M01 / moments.M00);
+
+                if (approximation.Size==3)
+                {
+                    CvInvoke.PutText(inputImage,"Trangle",new Point(x,y),Emgu.CV.CvEnum.FontFace.HersheyPlain,1,new MCvScalar(0,0,0),1);
+
+                }
+
+                if (approximation.Size==4)
+                {
+                    Rectangle rect = CvInvoke.BoundingRectangle(conturs[i]);
+                    double aspectRatio = (double)rect.Width / (double)rect.Height;
+
+                    if (aspectRatio >= 0.95 && aspectRatio <= 1.05)
+                    {
+                        CvInvoke.PutText(inputImage, "Square", new Point(x, y), Emgu.CV.CvEnum.FontFace.HersheyPlain, 1, new MCvScalar(0, 0, 0), 1);
+                    }
+                    else
+                    {
+                        CvInvoke.PutText(inputImage, "Rectangle", new Point(x, y), Emgu.CV.CvEnum.FontFace.HersheyPlain, 1, new MCvScalar(0, 0, 0), 1);
+                    }
+                }
+
+                if (approximation.Size == 5)
+                {
+                    CvInvoke.PutText(inputImage, "Pentagon", new Point(x, y), Emgu.CV.CvEnum.FontFace.HersheyPlain, 1, new MCvScalar(0, 0, 0), 1);
+
+                }
+                if (approximation.Size == 6)
+                {
+                    CvInvoke.PutText(inputImage, "Hexagon", new Point(x, y), Emgu.CV.CvEnum.FontFace.HersheyPlain, 1, new MCvScalar(0, 0, 0), 1);
+
+                }
+
+                if (approximation.Size > 6)
+                {
+                    CvInvoke.PutText(inputImage, "Circle", new Point(x, y), Emgu.CV.CvEnum.FontFace.HersheyPlain, 1, new MCvScalar(0, 0, 0), 1);
+
+                }
+
+                
+
+                
+
+                pictureBox2.Image = inputImage.Bitmap;
+            }
+        }
+    }
+}
